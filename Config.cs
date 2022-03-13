fd1286d83fafcccdc774624c705a71a48d37cbe8
Arnaldo Tadeu Chioquetta <arnaldo.chioquetta@hotmail.com>
2022-03-13 18:27:24 -0300

    Grava a configuração no INI

--- Config.cs
+++ Config.cs
@@ -12,6 +12,12 @@ namespace FTPc
 
         private void button1_Click(object sender, EventArgs e)
         {
+            INI2 MeuIni = new INI2();
+            MeuIni.WriteString("Config", "host", txHost.Text);
+            MeuIni.WriteString("Config", "user", txUser.Text);
+            MeuIni.WriteString("Config", "pass", txSenha.Text);
+            MeuIni.WriteString("Config", "CamLocal", txLocal.Text);
+            MeuIni.WriteString("Config", "PastaBaseFTP", txCamFTP.Text);
             Close();
         }
     }
